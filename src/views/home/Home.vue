efef476146717a67a9169a83fda3bdbe8820fc2d
inzaghibest <448435439@qq.com>
2020-06-16 17:25:18 +0800

    second day

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,9 +1,75 @@
 <template>
     <div id="home">
        <nav-bar class="home-nav">
-         <div slot="center">购物车</div>
+         <div slot="center">购物街</div>
        </nav-bar>
        <home-swiper :banners = "banners"/>
+       <remommend-view :recommends = "recommends"/>
+       <feature-view/>
+
+       <tab-control class="tab-control" :titles = "['流行',  '新款', '精选']"/>
+       <ul>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+         <li>列表1</li>
+       </ul>
     </div>
 </template>
 
@@ -11,28 +77,62 @@
 
 import NavBar from 'components/common/navbar/NavBar'
 import HomeSwiper from './childComps/HomeSwiper'
+import RemommendView from './childComps/RemommendView'
+import FeatureView from './childComps/FeatureView'
+import TabControl from 'components/content/tabControl/TabControl'
 
-import { getHomeMultiData } from 'network/home'
+import { getHomeMultiData, getHomeGoods } from 'network/home'
 
 
 export default {
   name: 'Home',
   components: {
     NavBar,
-    HomeSwiper
+    HomeSwiper,
+    RemommendView,
+    FeatureView,
+    TabControl
   },
   data () {
     return {
+      // 轮播图
       banners: [],
-      recommends: []
+      // 首页图片
+      recommends: [],
+      // 列表页
+      goods: {
+        'pop':  {page: 0, list: []},
+        'sell': {page: 0, list: []},
+        'new': {page: 0, list: []}
+      }
     }
   },
   created () {
-    getHomeMultiData().then(res => {
-      console.log(res)
-      this.banners = res.data.data.banner.list
-      this.recommends = res.data.data.recommend.ist
-    })
+    // 1.请求轮播图数据
+        this.getHomeMultiDataD();
+
+    // 2.请求首页数据
+        this.getHomeGoodsD('pop');
+        this.getHomeGoodsD('sell');
+        this.getHomeGoodsD('new');
+
+  },
+  methods: {
+    getHomeMultiDataD()
+    {
+      getHomeMultiData().then(res => {
+        this.banners = res.data.data.banner.list
+        this.recommends = res.data.data.recommend.list
+      })
+    },
+    getHomeGoodsD (type) {
+      let page = (this.goods[type].page) + 1
+      getHomeGoods(type, page).then(res=> {
+        console.log(res)
+        this.goods[type].list.push(...res.data.data.list)
+        this.goods[type].page += 1
+      })
+    }
   }
 }
 </script>
@@ -41,5 +141,19 @@ export default {
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+
+  #home {
+    padding-top: 44px;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
   }
 </style>
---
+++ src/views/home/childComps/RemommendView.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="recommond">
+        <div v-for="item in recommends" :key="item.label" class="recommond-item">
+            <a :href="item.link">
+                <img :src="item.image" alt="">
+                <div>{{ item.title }}</div>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RemommendView', 
+    props: {
+        recommends: {
+            type: Array,
+            default () {
+                return []
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .recommond {
+        display: flex;
+        text-align: center;
+        font-size: 14px;
+
+        padding: 10px 0px 20px;
+        border-bottom: 10px solid #eee;
+    }
+
+    .recommond-item {
+        flex: 1;
+    }
+
+    .recommond-item img {
+        width: 80px;
+        height: 80px;
+        margin-bottom: 20px;
+    }
+</style>
